e86ac06cbd6d89eba2c7463638b3d45f26a81b40
Croumi <gilles_thia@hotmail.fr>
2020-11-11 20:26:03 +0100

    db_filler change spoonacular endpoint

--- db_filler.rb
+++ db_filler.rb
@@ -16,7 +16,8 @@ def get_recipes(url:)
   request["Cookie"] = "__cfduid=dda66b8c926a8d426f391c05243f6bb031604771552"
 
   response = https.request(request)
-  raise "api_key_used" unless response.kind_of? Net::HTTPSuccess
+  raise 'recipe not found' if response.kind_of? Net::HTTPNotFound
+  raise "api_key used" if response.kind_of? Net::HTTPPaymentRequired
   response.read_body
 end
 
@@ -27,9 +28,7 @@ def post_request(url:, request_body:)
   request = Net::HTTP::Post.new(url)
   request["Content-Type"] = "application/x-www-form-urlencoded"
   request.body = request_body
-
   response = http.request(request)
-  puts response.code
   response.read_body
 end
 
@@ -69,21 +68,69 @@ def post_recipes(url:, recipes:)
   end
 end
 
+def post_recipe(url:, recipe:)
+  recipe = JSON.parse(recipe)
+  recipe_hash = Hash.new
+  recipe_hash['title'] = recipe['title']
+  recipe_hash['externalId'] = recipe['id']
+  recipe_hash['readyInMinutes'] = recipe['readyInMinutes']
+  recipe_hash['servings'] = recipe['servings']
+  recipe_hash['category'] = recipe['cuisines']
+  recipe_hash['dishTypes'] = recipe['dishTypes']
+  recipe_hash['instructions'] = ''
+  recipe_hash['materialNeeded'] = []
+  recipe['analyzedInstructions']&.each do |analyzedInstruction|
+    analyzedInstruction&.dig('steps')&.each do |step|
+      recipe_hash['instructions'] << "#{step['number']} #{step['step']}\n"
+      step.dig('equipment')&.each do |equipment|
+        recipe_hash['materialNeeded'] << equipment.dig('localizedName')
+      end
+      recipe_hash['materialNeeded'].uniq!
+    end
+  end
+
+  recipe_hash['ingredients'] = []
+  recipe['extendedIngredients']&.each do |ingredient|
+    ingredient_hash = Hash.new
+    ingredient_hash['nom'] = ingredient['name']
+    ingredient_hash['uniteMesure'] = ingredient['unit']
+    request_body = URI.encode_www_form(ingredient_hash)
+    posted_ingredient = post_request(url: "#{url}/ingredients", request_body: request_body)
+    recipe_hash['ingredients'] << JSON(posted_ingredient)['id']
+  end
+  recipe_hash['ingredients'].uniq!
+  recipe_hash['diets'] = recipe['diets']
+  recipe_hash['photopath'] = recipe['image']
+  request_body = URI.encode_www_form(recipe_hash)
+  post_request(url: "#{url}/recettes", request_body: request_body)
+end
+
+
 config = YAML.load_file('db_filler.yml')
 spoonacular_api_keys = config['spoonacular_api_keys']
 externalId_start = config['externalId_start']
+index = config['index_start']
 spoonacular_api_keys.each do |spoonacular_api_key|
+  puts "current api key : #{spoonacular_api_key}"
   begin
     loop do
-      spoonacular_url = URI("https://api.spoonacular.com/recipes/random?apiKey=#{spoonacular_api_key}&number=100")
-
-        recipes = get_recipes(url: spoonacular_url)
-
+      spoonacular_url = URI("https://api.spoonacular.com/recipes/#{index}/information?apiKey=#{spoonacular_api_key}")
+      index+=1
+      begin
+        recipe = get_recipes(url: spoonacular_url)
+      rescue StandardError => e
+        puts e.inspect
+        if e.message == 'recipe not found'
+          next
+        else
+          raise
+        end
+      end
       myLittleChef_server_IP = config['myLittleChef_server_IP']
-      post_recipes(url: myLittleChef_server_IP, recipes: recipes)
+      post_recipe(url: myLittleChef_server_IP, recipe: recipe)
     end
   rescue StandardError => e
     puts e.inspect
-    next
+    next if e.message == 'api_key used'
   end
 end
