72d4ae01525416c5bc8dea644fd4588a26d778e7
Ratatinator97 <asidiras.csi@gmail.com>
2020-11-17 19:13:59 +0100

    changed some settinngs

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import * as config from 'config';
 
 async function bootstrap() {
+  const serverConfig = config.get('server');
+  const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = process.env.PORT || serverConfig.port;
+  await app.listen(port);
+  console.log('Node environment is :', process.env.NODE_ENV);
+  console.log('Port is : ', port);
   console.log("Launched");
 }
 bootstrap();
