d8ac89d5a38c9b89989bbaed26dd31dfe8ddfef6
jzhao1998 <46528670+jzhao1998@users.noreply.github.com>
2020-11-18 22:47:07 +0800

    Update scrapy.py

--- scrapy.py
+++ scrapy.py
@@ -4,6 +4,7 @@ import xml
 import requests
 
 url="https://world.openfoodfacts.org/cgi/search.pl?action=process&search_terms=prepared%20food&sort_by=unique_scans_n&page_size=20&page="
+#instant food url:"https://world.openfoodfacts.org/cgi/search.pl?action=process&search_terms=instant food&sort_by=unique_scans_n&page_size=20&page="
 foodPageUrl="https://world.openfoodfacts.org"
 preparedFoodList=[]
 for i in range(1,60):
@@ -49,25 +50,42 @@ def foodNameList(foodUrlList):
         l.append(i[i.rfind("/")+1:].replace("-"," "))
     return l
 foodName=foodNameList(preparedFoodList)
-
-sqlFile=open("preparedfood.sql","w")
-for i in range(len(foodName)):
-    sqlFile.write("insert into recette (id, title,dishTypes) values ("+str(i)+",\""+foodName[i]+"\",\"PreparedFood\");\n")
-
 ingredientsList=[]
 ingredients=[]
 n=0
+urlApiRecette="http://api.apps.asidirasga.me/recettes"
+urlApiIngredient="http://api.apps.asidirasga.me/ingredients"
+print("begin update")
+
 for i in range(len(preparedFoodList)):
     f=requests.get(preparedFoodList[i])
     soup = BeautifulSoup(f.content, "lxml")
     k=soup.find_all("div",id="ingredients_list")
     ingredientsList.append(deleteLettersInBracketsAndPercent(k[0].text).split(","))
+    urlImage=soup.find_all("img",id="og_image")[0]["src"]
+    r = requests.get(urlImage,stream=True)
+    with open("img/"+str(i)+".jpg", 'wb') as fd:
+        for chunk in r.iter_content():
+            fd.write(chunk)
     for m in ingredientsList[i]:
         if(m not in ingredients):
-            sqlFile.write("insert into ingredient (id,nom) values ("+str(n)+",\""+m+"\");\n")
-            sqlFile.write("insert into recette_ingredients (recetteId,ingredientId) values("+str(i)+","+str(n)+");\n")
+            dataIngredient={
+                "nom":m,
+                "uniteMesure":"NoUnit"
+            }
+            f=requests.post(urlApiIngredient,dataIngredient)
+            print(f.text)
             ingredients.append(m)
             n+=1
         else:
-            loc=ingredients.index(m)
-            sqlFile.write("insert into recette_ingredients (recetteId,ingredientId) values("+str(i)+","+str(loc)+");\n")
+            requests.post(urlApiRecette,data)
+    data={
+            "title":foodName[i],
+            "readyInMinutes":"Prepared",
+            "servings":"1",
+            "ingredients":i+1850,
+            "externalId":"1",#je ne sais pas comment gerer externalid
+            "photopath":"img/"+str(i)+".jpg"
+        }
+    f=requests.post(urlApiRecette,data)
+    print(f.text)
