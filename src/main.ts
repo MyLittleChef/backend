1874bc5f7df74296e34ed8a740e8cfb4e378eda3
Croumi <gilles_thia@hotmail.fr>
2021-01-03 23:41:53 +0400

    hotfix to update database

--- src/main.ts
+++ src/main.ts
@@ -35,7 +35,7 @@ async function bootstrap() {
   app.use(
     rateLimit({
       windowMs: 15 * 60 * 1000, // 15 minutes
-      max: 1000, // limit each IP to 100 requests per windowMs
+      max: 100000, // limit each IP to 100 requests per windowMs
     }),
   );
   app.use(helmet());
